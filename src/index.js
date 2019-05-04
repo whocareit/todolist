23c9b324278ce86bba093de10ad713aae763edb6
liuhu <1176304452@qq.com>
2019-05-04 20:35:03 +0800

    todolist1

---
+++ src/index.js
@@ -0,0 +1,90 @@
+import {createStore} from 'redux';
+import React  from 'react';
+import ReactDOM from 'react-dom';
+
+
+const curState = {items:[],text:''};
+const  reducer = (state = curState,action) => {
+    switch(action.type){
+      case 'handleSubmit':
+        return state = (function() {
+          if(!state.text.length){
+            return;
+          }
+          const newItems = {
+            text: state.text,
+            id:Date.now()
+          }
+          state = {
+            items: state.items.concat(newItems),
+            text:''
+          }
+          console.log(state)
+          return state;
+        }());
+      case 'handleDelete':
+        return state = (function(){
+          state.items = state.items.filter((items) =>{
+          return items.id !== action.id;
+          })
+          return state;
+      }());
+      default: return state;
+    }
+}
+const store = createStore(reducer);
+
+function Counter(props){
+  return (<ul>
+          {props.items.map(items =>(
+          <li key = {items.id}
+           onClick = {props.handleDelete} 
+          >{items.text}</li>
+         ))}
+         </ul>);
+      
+}
+class TodoList extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+    <div>
+    <form onSubmit = {this.props.handleSubmit}>
+      <input
+      id = 'new-Todo'
+      onChange = {(e) => {
+        store.getState().text = e.target.value;
+      }}
+      />
+      <button type="submit">
+       ADD
+      </button>
+      <Counter items = {store.getState().items} 
+      handleDelete = {() => store.dispatch({type:'handleDelete',id:store.getState().items[0].id})}
+      /> 
+    </form>     
+    </div>
+    );
+  }
+}
+
+const render = () =>{
+    ReactDOM.render(
+      <TodoList
+        handleSubmit = {(e) => {store.dispatch({type: 'handleSubmit'});e.preventDefault()}}
+      />,
+      document.getElementById('root')
+    );
+}
+render();
+store.subscribe(render);
+
+//创建三个方法，用于去处理整个todolist
+//submit,change,delete,change方法写在，，，其余两个方法写在reducer里面。
+//书写两个组件，第一个组件用来todolist,第二个组件，用来输出整个的todoapp
+//整个过程所涉及到的步骤分别有，第一个，创建一个reducer，来实现其中的两个方法。其中需要注意的就是reducer中的参数和所返回回来的对象
+//第二个就是todolist中，如何去绑定组件中的方法，并且将之与当前状态的store给联系起来
+//第三就是在todoapp中，怎样去实现整个的事件绑定，如何将与当前的state联系起来。
+
